664e02b3c2cc2a665cab3df3368549aa16c59c06
Juan Trejos <79339661+JuanTrejos010@users.noreply.github.com>
2023-08-30 11:17:16 -0500

    Add files via upload

---
+++ PrimerCorte/Operaciones.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int Operacion;
+	float Num1, Num2, Resultado;
+	std::cout << "--- Operaciones básicas ---\n\n";
+	std::cout << "Escribe la operación por hacer.\n 1)Suma | 2)Resta | 3) Multiplicación |4)División |5)Potenciación |6)Radicación\n";
+	std::cin >> Operacion;
+	switch (Operacion){
+		case 1:
+			std::cout << "\nInserta el primer sumando.\n";
+			std::cin >> Num1;
+			std::cout << "Inserta el segundo sumando.\n";
+			std::cin >> Num2;
+			Resultado=Num1+Num2;
+			std::cout<< "\n\nEl resultado es de " << Resultado;
+			break;
+		case 2:
+			std::cout << "\nInserta el minuendo.\n";
+			std::cin >> Num1;
+			std::cout << "Inserta el sustraendo.\n";
+			std::cin >> Num2;
+			Resultado=Num1-Num2;
+			std::cout<< "\n\nEl resultado es de " << Resultado;
+			break;
+		case 3:
+			std::cout << "\nInserta el primer factor.\n";
+			std::cin >> Num1;
+			std::cout << "Inserta el segundo factor.\n";
+			std::cin >> Num2;
+			Resultado=Num1*Num2;
+			std::cout<< "\n\nEl resultado es de " << Resultado;
+			break;
+		case 4:
+			std::cout << "Inserta el dividendo.\n";
+			std::cin >>Num1;
+			std::cout << "Inserta el divisor.\n";
+			std::cin >> Num2;
+			Resultado=Num1/Num2;
+			std::cout<< "\n\nEl resultado es de " << Resultado;
+			break;
+		case 5:
+			std::cout << "Inserta la base.\n";
+			std::cin >>Num1;
+			std::cout << "Inserta el exponente.\n";
+			std::cin >> Num2;
+			Resultado=pow(Num1,Num2);
+			std::cout<< "\n\nEl resultado es de " << Resultado;
+			break;
+		case 6:
+			std::cout << "Inserta el radicando.";
+			std::cin >>Num1;
+			std::cout << "Inserta el índice.";
+			std::cin >> Num2;
+			Resultado=pow(Num1,1/Num2);
+			std::cout<< "El resultado es de " << Resultado;
+			break;
+		default:
+			std::cout<<"Operación no reconocida.";
+			break;
+	}
+	std::cout << "\n\n\nRecuerda que en el mundo hay genios y talentos.";
+}
