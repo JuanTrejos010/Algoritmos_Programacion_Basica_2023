859bfcaad93e58a6791d74468541d463fe10b97e
Juan Trejos <79339661+JuanTrejos010@users.noreply.github.com>
2023-09-06 11:48:42 -0500

    Create Tabla10.cpp

---
+++ PrimerCorte/Parcial/Tabla10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	int factor2, resultado;
+	for(factor2=1;factor2<=12;factor2++){
+		resultado=10*factor2;
+		std::cout << "10 * " << factor2 <<"= " << resultado << endl;
+	}
+}
