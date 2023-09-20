7630ef05b40ce8aea48aeff5e0d5bf3e5b6fd1b9
Juan Trejos <79339661+JuanTrejos010@users.noreply.github.com>
2023-09-20 00:44:06 -0500

    Update Numero Edades.cpp

--- SegundoCorte/Numero Edades.cpp
+++ SegundoCorte/Numero Edades.cpp
@@ -2,31 +2,31 @@
 
 int main(){
 	int NumP,Cont,Sumatoria=0, Promedio;
-	std::cout << "¿Cuántas personas hay?\n";
+	std::cout << "Â¿CuÃ¡ntas personas hay?\n";
 	std::cin >> NumP;
-	int Edades[NumP];
+	int Edades[NumP-1];
 	std::cout << "\n\n";
 	for (Cont=0; Cont<NumP; Cont+=1){
 		std::cout << "Edad\n";
 		std::cin >> Edades[Cont];
 		std::cout << "\n\n";
-		Sumatoria+=Edades[Cont];	
+		Sumatoria+=Edades[Cont];
 	}
 	Promedio=Sumatoria/NumP;
 	std::cout << "\nPromedio: " << Promedio << "\n";
+	int EdadMayor=Edades[0];
+	int EdadMenor=Edades[0];
 	for (Cont=0; Cont<NumP; Cont+=1){
 		std::cout << Edades[Cont] << " | ";
-	}
-	std::cout << "\n\n";
-	for (Cont=1; Cont<NumP; Cont+=1){
-		if(Edades[0]<Edades[Cont]){
-			Edades[0]=Edades[Cont];
+		if(Edades[0]>EdadMayor){
+			EdadMayor=Edades[Cont];
+		}
+		if(Edades[0]<EdadMenor){
+			EdadMenor=Edades[Cont];
 		}
 	}
 	std::cout << "\n\n";
-	for (Cont=0; Cont<=NumP; Cont+=1){
-		std::cout << Edades[Cont] << " | ";
-	}
 	std::cout << "\n\n";
-	std::cout << "\nEdad mayor: " << Edades[0];
+	std::cout << "Edad menor: " << EdadMenor;
+	std::cout << "\nEdad mayor: " << EdadMayor;
 }
